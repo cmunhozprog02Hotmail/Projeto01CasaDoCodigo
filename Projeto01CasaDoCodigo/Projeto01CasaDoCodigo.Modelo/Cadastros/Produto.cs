b2e82e17b2836a32ffd9338bf982d19a49a18e4f
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-07-11 18:37:18 -0300

    riaçao de classes

---
+++ Projeto01CasaDoCodigo/Projeto01CasaDoCodigo.Modelo/Cadastros/Produto.cs
@@ -0,0 +1,30 @@
+﻿using Projeto01CasaDoCodigo.Modelo.Tabelas;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto01CasaDoCodigo.Modelo.Cadastros
+{
+    public class Produto
+    {
+        
+        public long? ProdutoId { get; set; }
+
+       
+        public string Nome { get; set; }
+
+        [DisplayName("Categoria")]
+        public long? CategoriaId { get; set; }
+
+        [DisplayName("Fabricante")]
+        public long? FabricanteId { get; set; }
+
+
+        public Categoria Categoria { get; set; }
+        public Fabricante Fabricante { get; set; }
+    }
+}
+}
